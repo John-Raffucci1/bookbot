89a8cecdd177a3c7f0889f86c7464b180da1d840
John Raffucci <john.raffucci1@gmail.com>
2024-08-12 13:19:34 -0700

    fixed code and made it functional

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 def main():
-    with open('books/frankenstein.txt') as f:
+    book_path = "books/frankenstein.txt"
+
+    book = get_book_path(book_path)
+
+    book_chars = count_characters(book)
+
+    new_list = list(book_chars.items())
+
+    count = letter_count(book)
+
+    # start of report 
+    print("--- Begin report of book ---")
+    print(f"{count} words found in the document")
+    for char in new_list:
+        if char[0].isalpha() == True:
+            letter = char[0]
+            number = char[1]
+            print(f"The {letter} character was found {number} times")
+    print("--- End report ---")
+def get_book_path(path):
+    with open(path) as f:
         file_contents = f.read()
-        return print("working!")
+        return file_contents
 def letter_count(str):
     words = str.split()
     word_number = 0
@@ -18,19 +38,4 @@ def count_characters(book):
         if char in char_set: 
             char_set[char] = 1 + char_set[char]
     return char_set
-def report(prev_book_chars,book):
-    book_chars = count_characters(prev_book_chars)
-    new_list = list(book_chars.items())
-    count = letter_count(book)
-    print("--- Begin report of book ---")
-    print(f"{count} words found in the document")
-    for char in new_list:
-        if char[0].isalpha() == True:
-            letter = char[0]
-            number = char[1]
-            print(f"The {letter} character was found {number} times")
-    print("--- End report ---")
-count_characters("Hello World")         
-letter_count("Hello World")
-report("Hello World","Hello World")
 main()
